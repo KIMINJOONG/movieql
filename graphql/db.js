7c6b9580be42de9aa210d85194387c96abcbdda5
KIMINJOONG <ijboym216@gmail.com>
2019-03-17 09:33:22 +0900

    Delete Mutation

--- graphql/db.js
+++ graphql/db.js
@@ -1,21 +1,21 @@
 let movies = [
     {
-        id: "0",
+        id: 0,
         name: "Start Wars - The new one",
         score: 1
     },
     {
-        id: "1",
+        id: 1,
         name: "Avengers = The new one",
         score: 8
     },
     {  
-        id: "2",
+        id: 2,
         name: "The Godfather I",
         score: 99
     },
     {   
-        id: "3",
+        id: 3,
         name: "Logan",
         score: 2
     }
@@ -24,14 +24,14 @@ let movies = [
 export const getMovies = () => movies;
 
 export const getById = id => {
-    const filteredPeople = people.filter(person => person.id === String(id));
+    const filteredPeople = people.filter(person => person.id === id);
     return filteredPeople[0];
 };
 
 export const deleteMovie = (id) => {
-    const cleanedMovies = movies.filter(movie => movie.id !== String(id));
+    const cleanedMovies = movies.filter(movie => movie.id !== id);
     if(movies.length > cleanedMovies.length) {
-        movie = cleanedMovies;
+        movies = cleanedMovies;
         return true;
     } else {
         return false;
